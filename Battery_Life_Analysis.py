d4bfa9b88ced23f15b3f787715c21e43e320d889
Michael Keller <michaelkeller1997@gmail.com>
2020-04-06 13:36:36 -0400

    Added Tx and RasPi information

--- Battery_Life_Analysis.py
+++ Battery_Life_Analysis.py
@@ -2,6 +2,15 @@
 motorDrawMin = 0.52 # amps
 motorDrawMax = 20 # amps 
 
-#battery 
+# battery 
 capacity = 8000 #mAh
 volts = 11.1 #volts 
+
+# Raspberry Pi
+raspiCurrent = 2.5 #A
+raspiVolts = 5 #volts 
+
+# Transmitter
+txCurrent = 220 #mA
+txVotls = 12 #volts
+
